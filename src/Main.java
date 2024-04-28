0f1281e50689dcc133ac6497239177fabac55863
Tatyana <tatyanalipch@yandex.ru>
2024-04-28 21:52:13 +0300

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int x = 13676;// стоимость билета
-        int y = 20;// бонусы за одну милю
-        int z = x / y;
-        System.out.println(z);
+        int stoimBil = 13676;// стоимость билета
+        int bonZaMil = 20;// бонусы за одну милю
+        int kolMlZaBil = stoimBil / bonZaMil;
+        System.out.println(kolMlZaBil);
     }
 }
